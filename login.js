238fc075d5085b29f951c944a3e2265f889a7720
allenchrist <727723euec016@skcet.ac.in>
2025-01-23 10:03:10 +0530

    demo

---
+++ login.js
@@ -0,0 +1,23 @@
+function handlelogin()
+{
+    const username=document.getElementById('user').Value;
+    const password=document.getElementById('pass').Value;
+    const error=document.getElementById('error');
+
+
+const logindata=[{username:"cristiano",password:"pass123"},
+                  {username:"virat",password:"123pass"},
+                  {username:"username",password:"password"},
+
+]
+
+const isValid=logindata.some((user)=>
+user.username===username && user.password===password)
+if(isValid){
+    window.location.href='positioning.html';
+    error.innerText='login successful';
+}
+else{
+    error.innerText="Invalid credentials";
+}
+}
