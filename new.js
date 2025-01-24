a5b0028b554d7cd3a832eeab2de6e316f7dc3036
allenchrist <727723euec016@skcet.ac.in>
2025-01-24 15:03:11 +0530

    demo

---
+++ new.js
@@ -0,0 +1,14 @@
+let user={
+    name:"Allen",
+    age:19,
+    "is singer":true,
+}
+for(let key in user)
+{
+    console.log(key+":"+user[key]);
+}
+let array=[1,2,3,4,5,6]
+
+for(let value of array){
+    console.log(value);
+}
